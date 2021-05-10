d104a3cd5115006c36df09841aed2259fae1a3e6
shvmsh20 <itsshivamsharma21@gmail.com>
2021-05-10 19:53:53 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+## Creative Agency
+A simple website build using HTML, CSS and a little bit of Javascript.
---
+++ js/script.js
@@ -0,0 +1,4 @@
+document.querySelector(".toggle").addEventListener("click",function(){
+   document.querySelector(".navigation").classList.toggle("active");
+    document.querySelector(".toggle").classList.toggle("active");
+ })
